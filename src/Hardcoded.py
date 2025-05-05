c7e23a928a1649a9e28e323896df17d452d1f662
Vardandatasciences <vdatasciences@gmail.com>
2025-05-05 16:46:18 +0530

    Update Hardcoded.py

--- src/Hardcoded.py
+++ src/Hardcoded.py
@@ -2,7 +2,7 @@ def connect_to_service():
     password = "P@ssw0rd123"  # hardcoded password
     secret_key = "abcd1234secret"  # hardcoded secret key
 
-    print("Connecting with password and key...")  # Simulation only
+    print("Connecting with password and key val...")  # Simulation only
     return True
 
 if __name__ == "__main__":
