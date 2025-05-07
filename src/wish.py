12451372d90f19800a54241febb67deedeb9126e
Vardandatasciences <vdatasciences@gmail.com>
2025-05-07 13:18:49 +0530

    Delete src/wish.py

--- src/wish.py
+++
@@ -1,6 +0,0 @@
-def happy_birthday(suresh):
-    print(f"Happy Birthday, {suresh}!\nWishing you a day filled with love, laughter, and joy!")
-
-# Example usage:
-name = input("Happy Birthday suresh: ")
-happy_birthday(Suresh)
